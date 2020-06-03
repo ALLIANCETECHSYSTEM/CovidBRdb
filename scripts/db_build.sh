708bc99ce494435f4fd4682c85a021657af049e0
Rafael Sabino <rafael.sabino@hotmail.com.br>
2020-06-03 14:24:32 -0300

    Add files via upload

--- scripts/db_build.sh
+++ scripts/db_build.sh
@@ -5,8 +5,8 @@
 # sqlite3 - sudo apt-get install sqlite3
 
 ############## 
-# Setup your workspace
-#DB_PATH=databases/sqlite/covid19BRdb.sqlite3 ### Passed as ENV
+# Setup our workspace
+#DB_PATH=databases/sqlite/covid19WLDdb.sqlite3 ### Passed as ENV
 
 #################
 # Build the database Covid_BR
@@ -14,14 +14,79 @@
 # First remove the existing database file, if any
 rm -f $DB_PATH
 
+## Create the wrldcovid_all table
+echo "CREATE TABLE wrldcovid_all (
+  Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+  Cases INTEGER,
+  Todaycases INTEGER,
+  Deaths INTEGER,
+  Todaydeaths INTEGER,
+  Recovered INTEGER,
+  Active INTEGER,
+  Critical INTEGER,
+  Casesperonemillion INTEGER,
+  Deathsperonemillion INTEGER,
+  Tests INTEGER,
+  Testsperonemillion NUMBER,
+  Population INTEGER,
+  Activeperonemillion NUMBER,
+  Recoveredperonemillion NUMBER,
+  Criticalperonemillion NUMBER,
+  Affectedcountries INTEGER
+);" | sqlite3 $DB_PATH
 
-## Create the covid_regions table
-echo "CREATE TABLE covid_regions (
-  IBGE REAL, 
-  UF VARCHAR, 
-  Regiao VARCHAR,
-  Qtd_Mun REAL
-  
+## Create the wrldcovid_counties table
+echo "CREATE TABLE wrldcovid_counties (
+  Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+  Cases INTEGER,
+  Todaycases INTEGER,
+  Deaths INTEGER,
+  Todaydeaths INTEGER,
+  Recovered INTEGER,
+  Active INTEGER,
+  Critical INTEGER,
+  Casesperonemillion NUMBER,
+  Deathsperonemillion NUMBER,
+  Tests INTEGER,
+  Testsperonemillion NUMBER,
+  Population INTEGER,
+  Continent VARCHAR,
+  Activeperonemillion NUMBER,
+  Recoveredperonemillion NUMBER,
+  Criticalperonemillion NUMBER,
+  Countries VARCHAR
+);" | sqlite3 $DB_PATH
+
+## Create the wrldcovid_countries table
+echo "CREATE TABLE wrldcovid_countries (
+  Updated DATETIME DEFAULT CURRENT_TIMESTAMP,
+  Country VARCHAR,
+  Cases INTEGER,
+  Todaycases INTEGER,
+  Deaths INTEGER,
+  Todaydeaths INTEGER,
+  Recovered INTEGER,
+  Active INTEGER,
+  Critical INTEGER,
+  Casesperonemillion INTEGER,
+  Deathsperonemillion INTEGER,
+  Tests INTEGER,
+  Testsperonemillion NUMBER,
+  Population INTEGER,
+  Continent VARCHAR,
+  Activeperonemillion NUMBER,
+  Recoveredperonemillion NUMBER,
+  Criticalperonemillion NUMBER
+);" | sqlite3 $DB_PATH
+
+## Create the wrldcovid_hist table
+echo "CREATE TABLE wrldcovid_hist (
+  Date DATE, 
+  Country VARCHAR,
+  Province VARCHAR,
+  Cases INTEGER,
+  Recovered INTEGER,
+  Deaths INTEGER
 );" | sqlite3 $DB_PATH
 
 #####################
--- scripts/db_populate.sh
+++ scripts/db_populate.sh
@@ -5,22 +5,39 @@
 # csvkit - sudo pip install csvkit
 
 ############## 
-# Setup databese file
-#DB_PATH=databases/sqlite/covid19BRdb.sqlite3 ### Passed as ENV
-CSV_DIR=files/covid19br
+# Setup new data workspace | Alliance Tech System
+#DB_PATH=databases/sqlite/covid19WLDdb.sqlite3 ### Passed as ENV
+CSV_DIR=/tmp/covid19wld
 mkdir -p $CSV_DIR
 
 ################ 
 # Download the files into $CSV_DIR
-curl 'https://brasilcontracovid19.com.br/files/covid19_region_2.csv' \
-      -o $CSV_DIR/covidbr-regions.csv
+curl 'https://docs.google.com/spreadsheets/d/140m-MGvRmfyku3rT0qwflfTyobm2MZSK3-m4IBYuttw/export?format=csv&id=140m-MGvRmfyku3rT0qwflfTyobm2MZSK3-m4IBYuttw&gid=510014428' \
+      -o $CSV_DIR/covidwld-all.csv
+curl 'https://docs.google.com/spreadsheets/d/140m-MGvRmfyku3rT0qwflfTyobm2MZSK3-m4IBYuttw/export?format=csv&id=140m-MGvRmfyku3rT0qwflfTyobm2MZSK3-m4IBYuttw&gid=1116965246' \
+      -o $CSV_DIR/covidwld-counties.csv
+curl 'https://docs.google.com/spreadsheets/d/140m-MGvRmfyku3rT0qwflfTyobm2MZSK3-m4IBYuttw/export?format=csv&id=140m-MGvRmfyku3rT0qwflfTyobm2MZSK3-m4IBYuttw&gid=1287373792' \
+      -o $CSV_DIR/covidwld-countries.csv
+curl 'https://raw.githubusercontent.com/flavianogjc/covid_19/master/countries_data.csv' \
+      -o $CSV_DIR/covidwld-historical.csv
+
 #####################
 # Insert the data
 
-## Insert the andamento_nazionale data
-csvsql $CSV_DIR/covidbr-regions.csv  \
+## Insert the tables data
+csvsql $CSV_DIR/covidwld-all.csv  \
     --db sqlite:///$DB_PATH --insert --no-create \
-    --tables covid_regions
+    --tables wrldcovid_all   
+csvsql $CSV_DIR/covidwld-counties.csv  \
+    --db sqlite:///$DB_PATH --insert --no-create \
+    --tables wrldcovid_counties   
+csvsql $CSV_DIR/covidwld-countries.csv  \
+    --db sqlite:///$DB_PATH --insert --no-create \
+    --tables wrldcovid_countries    
+csvsql $CSV_DIR/covidwld-historical.csv  \
+    --db sqlite:///$DB_PATH --insert --no-create \
+    --tables wrldcovid_hist
+
 #####################
 # EXIT
 exit 0
