b725777eef8fef72a7d396991a8cdcddbb5898fa
Rafael Sabino <rafael.sabino@hotmail.com.br>
2020-09-17 16:13:59 -0300

    Update DB
    Update files CSV

--- scripts/db_populate.sh
+++ scripts/db_populate.sh
@@ -5,7 +5,7 @@
 # csvkit - sudo pip install csvkit
 
 ############## 
-# Setup databese file
+# Setup database file
 #DB_PATH=databases/sqlite/covid19BRdb.sqlite3 ### Passed as ENV
 CSV_DIR=/tmp/covid19br
 mkdir -p $CSV_DIR
